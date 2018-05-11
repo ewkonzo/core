9b62dd0916c1787aefeebe2858371034788bdb62
Eliya Wolfram Konzo <ewkonzo@gmail.com>
2018-05-11 09:21:01 +0300

    Added Ionic input selector

--- src/core/src/directives/keyboard.directive.ts
+++ src/core/src/directives/keyboard.directive.ts
@@ -6,7 +6,7 @@ import { MatKeyboardComponent } from '../components/keyboard/keyboard.component'
 import { MatKeyboardService } from '../services/keyboard.service';
 
 @Directive({
-  selector: 'input[matKeyboard], textarea[matKeyboard]'
+  selector: 'input[matKeyboard], textarea[matKeyboard], ion-input[matKeyboard], ion-textarea[matKeyboard]'
 })
 export class MatKeyboardDirective implements OnDestroy {
 
